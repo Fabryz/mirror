e4be4ca171caba2cc3595b34dfaf66097f104fab
Fabrizio Codello <fabryz@gmail.com>
2012-03-26 15:28:51 +0200

    added websocket functionalities, streaming current video to all

--- public/js/mirror.js
+++ public/js/mirror.js
@@ -20,6 +20,25 @@ $(document).ready(function() {
 		}
 	}
 
+	function renderData(data) {
+		var col = data.split(";");
+		var img = image;
+
+		for (var i = 0; i < canvasWidth; i++) {
+			var tmp = parseInt(col[i]);
+			img.data[pos + 0] = (tmp >> 16) & 0xff;
+			img.data[pos + 1] = (tmp >> 8) & 0xff;
+			img.data[pos + 2] = tmp & 0xff;
+			img.data[pos + 3] = 0xff;
+			pos+= 4;
+		}
+
+		if (pos >= 4 * canvasWidth * canvasHeight) {
+			ctx.putImageData(img, 0, 0);
+			pos = 0;
+		}
+	}
+
 	function init() {
 		ctx.fillStyle = 'rgb(0, 0, 0)';
 		ctx.font = "15px Monospace";
@@ -29,34 +48,18 @@ $(document).ready(function() {
 
 		//ctx.fillText("Canvas ready.", 10, 20);
 
-		$("#webcam").webcam({
+		webcam_handle.webcam({
 			width: canvasWidth,
 			height: canvasHeight,
-			mode: "callback",
+			mode: "stream",
 			swffile: "js/jQuery-webcam/jscam_canvas_only.swf",
 			onTick: function() {
 
 			},
 			onSave: function(data) {
 
-				var col = data.split(";");
-				var img = image;
-
-				for (var i = 0; i < canvasWidth; i++) {
-					var tmp = parseInt(col[i]);
-					img.data[pos + 0] = (tmp >> 16) & 0xff;
-					img.data[pos + 1] = (tmp >> 8) & 0xff;
-					img.data[pos + 2] = tmp & 0xff;
-					img.data[pos + 3] = 0xff;
-					pos+= 4;
-				}
-
-				if (pos >= 4 * canvasWidth * canvasHeight) {
-					ctx.putImageData(img, 0, 0);
-					pos = 0;
-				}
-
-
+				socket.emit("screen", { screen: data });
+				log("Screen sent.");
 			},
 			onCapture: function() {
 				webcam.save();
@@ -73,13 +76,14 @@ $(document).ready(function() {
 			}
 		});
 
+		//webcam_handle.attr({ width: canvasWidth, height: canvasHeight });
 
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-		var img = new Image();
-		img.src = "http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png";
-		img.onload = function() {
-			ctx.drawImage(img, canvasWidth / 2 - img.width / 2, canvasHeight / 2 - img.height / 2);
+		var temp_img = new Image();
+		temp_img.src = "http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png";
+		temp_img.onload = function() {
+			ctx.drawImage(temp_img, canvasWidth / 2 - temp_img.width / 2, canvasHeight / 2 - temp_img.height / 2);
 		}
 		image = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
@@ -118,9 +122,9 @@ $(document).ready(function() {
 		canvasWidth = canvas.width(),
 		canvasHeight = canvas.height();
 
-	var pos = 0;
-	var cam = null;
-	var image = null;
+	var webcam_handle = $("#webcam"),
+		pos = 0,
+		image = null;
 
 	log(canvasWidth +"x"+ canvasHeight +" Canvas ready.");
 	init();
@@ -147,8 +151,10 @@ $(document).ready(function() {
 		tot.html(data.tot);
 	});
 
-	socket.on('default', function(data) {	
-		//log(data);
+	socket.on('screen', function(data) {	
+		log("Screen received");
+
+		renderData(data.screen);
 
 	});
 });
